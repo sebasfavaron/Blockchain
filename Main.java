69298a9af9d785997e3b5365c073eb6b3012315d
navi <rnavarro@itba.edu.ar>
2017-09-25 16:16:12 -0300

    ola

--- Main.java
+++ Main.java
@@ -17,9 +17,10 @@ public class Main {
         /* Constructing tree given in the above figure */
         tree.insert(5);
         tree.insert(7);
-        tree.insert(3);
         tree.insert(2);
-        tree.insert(1);
+        tree.insert(4);
+        tree.insert(3);
+
 
         /* The constructed AVL Tree would be
              30
