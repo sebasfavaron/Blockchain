e3a5383534be855ee02f978ff5f3f738e8f6d4ec
sebasfavaron <sebas.favaron@hotmail.com>
2017-09-26 23:35:41 -0300

    limpie un poco MyLinkedList

--- BlockChain.java
+++ BlockChain.java
@@ -9,13 +9,13 @@ public class BlockChain<T> {
     private MyLinkedList<Block> cadena;
     private int index;
     private Comparator<T> cmp;
-    private Integer zero;
+    private Integer amountZeroes;
     public BlockChain(Comparator<T> cmp){
         index=0;
         this.cmp=cmp;
     }
-    public void zeros(Integer zero){
-        this.zero=zero;
+    public void setAmountZeroes(Integer amountZeroes){
+        this.amountZeroes = amountZeroes;
     }
 
     public void add(T elem){
@@ -35,26 +35,33 @@ public class BlockChain<T> {
         private AvlTree<T> tree;
 
         public Block(T elem,Comparator<T> cmp) {
-            this.indice=++index;
-            this.data=elem;
-            this.tree=new AvlTree<>(cmp);
+            this.indice = ++index;
+            this.data = elem; //queremos poner al elem directo o un mensaje onda "insert 'elem'"?
+            this.tree = new AvlTree<>(cmp);
             this.tree.insert(elem);
-            this.prev="0";
+            this.prev = "0";
         }
         public void mine(){
             String aux = "";
-            for(int i=0; i<zero; i++){
+            for(int i=0; i<amountZeroes; i++){
                 aux = aux.concat("0");
             }
-            System.out.println(aux);
 
             //checks if hash starts with the required amount of zeroes, updates it if it doesn't
-            while(hash.substring(0,zero).equals(aux)){
-                updateHash();
+            while(hash.substring(0,amountZeroes).equals(aux)){
+                hash = ((Integer) hashCode()).toString(); //lo hice re villero, fijense como hacerlo bien. no se porque hash es String y no int
+                nonce++;
+                System.out.println(hash);
             }
         }
-        private void updateHash(){
-            return;
+        //Calculates the hash
+        public int hashCode(){
+            int result = 1;
+            result = 31 * result + indice;
+            result = 31 * result + nonce;
+            result = 31 * result + data.hashCode();
+            result = 31 * result + prev.hashCode();
+            return result;
         }
     }
 }
