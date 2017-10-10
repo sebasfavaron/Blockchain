c5d761b1336ae2fbdf1198ccf79280e67367e6fe
pabloito <pradnic@itba.edu.ar>
2017-10-09 22:27:36 -0300

    Removed past state

--- src/Blockchain/Main.java
+++ src/Blockchain/Main.java
@@ -12,48 +12,8 @@ public class Main {
     public static void main(String[]args){
 
     	paramsManager(args);
-
-    	
-//    	AvlTree<Integer> tree = new AvlTree<>(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                return o1-o2;
-//            }
-//        });
-//
-//        /* Constructing tree given in the above figure */
-//        tree.insert(5);
-//        tree.insert(7);
-//        tree.insert(3);
-//        tree.insert(4);
-//        tree.insert(2);
-//        tree.remove(7);
-//
-//
-//
-//        /* The constructed AVL Tree would be
-//             30
-//            /  \
-//          20   40
-//         /  \     \
-//        10  25    50
-//        */
-//        System.out.println("Preorder traversal" +
-//                " of constructed tree is : ");
-//        tree.print();
-//        BlockChain<Integer> blockChain = new BlockChain<>(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                return o1-o2;
-//            }
-//        });
-//        blockChain.setAmountZeroes(2);
-//        blockChain.add(1);
-//        blockChain.add(2);
-//        blockChain.print();
     }
-    
-    
+       
     
     // Separate method to avoid messing up the main
     private static void paramsManager(String[] args) {
