6813fbdf34535007f5cf9f685a6f6204065de08c
navi <rnavarro@itba.edu.ar>
2017-10-11 11:10:47 -0300

    Test

---
+++ src/Blockchain/AvlTreeTest.java
@@ -0,0 +1,65 @@
+package src.Blockchain;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class AvlTreeTest {
+    @org.junit.Test
+    public void height() throws Exception {
+    }
+
+    @org.junit.Test
+    public void contains() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getBalance() throws Exception {
+    }
+
+    @org.junit.Test
+    public void insert() throws Exception {
+    }
+
+    @org.junit.Test
+    public void remove() throws Exception {
+    }
+
+    @org.junit.Test
+    public void preOrder() throws Exception {
+    }
+
+    @org.junit.Test
+    public void preOrderHash() throws Exception {
+    }
+
+    @org.junit.Test
+    public void print() throws Exception {
+    }
+
+    @org.junit.Test
+    public Object clone() {
+        return null;
+    }
+
+    @org.junit.Test
+    public void cloneR() throws Exception {
+    }
+
+    @org.junit.Test
+    public int hashCode() {
+        return 0;
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(AvlTree.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
---
+++ src/Blockchain/HexaTest.java
@@ -0,0 +1,49 @@
+package src.Blockchain;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class HexaTest {
+    @Test
+    public void inc() throws Exception {
+    }
+
+    @Test
+    public void check() throws Exception {
+    }
+
+    @Test
+    public void sha256() throws Exception {
+    }
+
+    @Test
+    public void getNonce() throws Exception {
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+
+    @Test
+    public void getHexaNumber() throws Exception {
+    }
+
+    @Test
+    public void getConcatData() throws Exception {
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(Hexa.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
