043ee66c5d63c1a61638a23ec46cc634010d3a12
navi <rnavarro@itba.edu.ar>
2017-10-11 21:47:27 -0300

    Test

--- src/Blockchain/Test/MyTest.java
+++ src/Blockchain/Test/MyTest.java
@@ -26,9 +26,26 @@ public class MyTest {
     @Test
     public void insertTest(){
         avlTree.insert(1);
-        assertEquals(true,avlTree.contains(1));
+        assertEquals("1",avlTree.print());
         avlTree.insert(2);
         avlTree.insert(3);
         assertEquals("2(1,3)",avlTree.print());
+        avlTree=new AvlTree<>(comparator);
+        avlTree.insert(3);
+        avlTree.insert(2);
+        avlTree.insert(1);
+        assertEquals("2(1,3)",avlTree.print());
+        avlTree.insert(4);
+        avlTree.insert(5);
+        avlTree.insert(6);
+        assertEquals("4(2(1,3),5(6r))",avlTree.print());
+        avlTree=new AvlTree<>(comparator);
+        avlTree.insert(6);
+        avlTree.insert(5);
+        avlTree.insert(4);
+        avlTree.insert(3);
+        avlTree.insert(2);
+        avlTree.insert(1);
+        assertEquals("3(2(1l),5(4,6))",avlTree.print());
     }
 }
