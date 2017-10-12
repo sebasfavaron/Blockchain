51240b72ea3b0a6fbe2a0c3bb93d320b89903fbc
sebasfavaron <sebas.favaron@hotmail.com>
2017-10-11 21:13:25 -0300

    Update BlockChain.java

--- src/Blockchain/BlockChain.java
+++ src/Blockchain/BlockChain.java
@@ -8,7 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -19,9 +19,8 @@ import java.util.Scanner;
  */
 public class BlockChain<T> {
 
-    private ArrayList<Block> cadena;
-    private int index;
-    private Comparator<T> cmp;
+    private List<Block> chain;
+    private Integer index;
     private Integer amountZeroes;
     private AvlTree<T> tree;
 
@@ -29,7 +28,7 @@ public class BlockChain<T> {
         index = 0;
         this.tree = new AvlTree<>(cmp);
         this.cmp = cmp;
-        this.cadena = new ArrayList<>();
+        this.chain = new ArrayList<>();
     }
 
     /**
@@ -40,15 +39,15 @@ public class BlockChain<T> {
      * @throws FileNotFoundException
      */
     public boolean modifyByIndex(int index, File file) throws FileNotFoundException {
-    	if (index > cadena.size()) {
+    	if (index > chain.size()) {
     		return false;
     	}
     	try {
-            cadena.get(index - 1).data = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+            chain.get(index - 1).data = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
         } catch (IOException e) {
             System.out.println(e);
         }
-        cadena.get(index-1).rehash();
+        chain.get(index-1).rehash();
         return true;
     }
 
@@ -59,7 +58,7 @@ public class BlockChain<T> {
      */
     public ArrayList<Integer> getBlockIndexes(int elem) {
         ArrayList<Integer> ret = new ArrayList<>();
-        for (Block each: cadena) {
+        for (Block each: chain) {
             if (each.elem.equals(elem)) {
                 ret.add(each.indice);
             }
@@ -76,10 +75,18 @@ public class BlockChain<T> {
     }
 
     public void add(T elem, String data, AvlTree<T> tree){
-        if(cadena.size() == 0){
-            cadena.add(new Block("0", data, elem, tree));
+        if(chain.size() == 0){
+            chain.add(new Block("0", data, elem, tree));
         } else {
-            cadena.add(new Block(cadena.get(cadena.size() - 1).hash.getHexaNumber(), data, elem, tree));
+            chain.add(new Block(chain.get(chain.size() - 1).hash.getHexaNumber(), data, elem, tree));
+        }
+    }
+    
+    public void add(T elem, String data, AvlTree<T> tree, Integer nonce, String hexa, String prevHexa){
+        if(chain.size() == 0){
+            chain.add(new Block("0", data, elem, tree, nonce, hexa));
+        } else {
+            chain.add(new Block(prevHexa, data, elem, tree, nonce, hexa));
         }
     }
 
@@ -95,7 +102,7 @@ public class BlockChain<T> {
     		zeroes += "0";
     	}
     	
-    	for (Block block: cadena) {
+    	for (Block block: chain) {
     		if (!block.hash.getHexaNumber().startsWith(zeroes)) {
     			return false;
     		}
@@ -111,12 +118,13 @@ public class BlockChain<T> {
     /**
      *
      */
-    public void print(){
-        for (Block block:cadena) {
-            System.out.println("-------------------------");
-            System.out.println(block);
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        for(Block block : chain) {
+            ret.append(block.toString()).append("-------------------------\n");
         }
-        System.out.println("Tree: "+tree.print());
+        ret.append("Index: "+index+"\n").append("AmountZeroes: "+amountZeroes+"\n").append("Tree: " + tree.print());
+        return ret.toString();
     }
 
     /**
@@ -125,6 +133,17 @@ public class BlockChain<T> {
      */
     public AvlTree<T> getTree() {
         return tree;
+    }
+    
+    // Overwrites all previous data
+    public void setProperties(Integer index, Integer amountZeroes, AvlTree<T> tree) {
+        this.index = index;
+        this.amountZeroes = amountZeroes;
+        this.tree = tree;
+    }
+
+    public void resetChain() {
+        chain = new ArrayList<>();
     }
 
     private class Block {
@@ -137,10 +156,9 @@ public class BlockChain<T> {
         private T elem;
 
         public Block(String prevHexa, String data, T elem, AvlTree<T> tree) {
-            //En elem se guarda el dato concreto
             this.elem = elem;
             this.indice = ++index;
-            //En data se guarda la info de la operacion
+            ////Data stores the type of operation performed
             this.data = data;
             this.prevHexa = prevHexa;
             this.nonce = 0;
@@ -148,6 +166,17 @@ public class BlockChain<T> {
             String concatData = indice.toString() + data + prevHexa + "." + nonce.toString(); //le pongo un '.' para reemplazar el nonce mas facil
             this.hash = new Hexa(concatData);
             mine();
+        }
+        
+        public Block(String prevHexa, String data, T elem, AvlTree<T> tree, Integer nonce, String hexa) {
+            this.prevHexa = prevHexa;
+            //Data stores the type of operation performed
+            this.data = data;
+            this.elem = elem;
+            this.tree = tree;
+            this.nonce = nonce;
+            this.hash = new Hexa(hexa, nonce);
+            this.indice = ++index;
         }
 
         public void mine(){
@@ -168,12 +197,13 @@ public class BlockChain<T> {
         }
 
         public String toString(){
-            return  "Index = "+ indice.toString() + '\n' + 
-                    "Nonce = "+ nonce.toString() + '\n' +
-                    "Data = "+ data+ '\n' +
-                    "Previous = "+ prevHexa+ '\n' +
-                    "HashCode = "+ hash.getHexaNumber()+ '\n' +
-                    "Tree = "+ tree.print();
+            return "Index: "+ indice.toString() + '\n' +
+                   "Nonce: "+ nonce.toString() + '\n' +
+                   "Tree: "+ tree.print()+ '\n' +
+                   "Previous: "+ prevHexa+ '\n' +
+                   "HashCode: "+ hash.getHexaNumber()+ '\n' +
+                   "Elem: "+ elem.toString() + '\n' +
+                   "Data: "+ data + '\n';
         }
     }
     private class Hexa {
